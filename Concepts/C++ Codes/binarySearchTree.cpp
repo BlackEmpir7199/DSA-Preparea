93cb7bd0aeccf1eefc96d6ab44455d6c2eafe92f
BlackEmpir7199 <blackempire9177@gmail.com>
2025-07-25 00:24:53 +0530

    added bst insert & inorderee

--- Concepts/C++ Codes/binarySearchTree.cpp
+++ Concepts/C++ Codes/binarySearchTree.cpp
@@ -15,7 +15,71 @@ class Node{
 };
 
 
-int main(){
 
+
+
+// print in sorted order
+void inOrder(Node* root){
+    if(!root) return;
+    inOrder(root->left);
+    cout << root->val << " ";
+    inOrder(root->right);
+}
+
+
+// insert func using recursion
+Node* insert(Node* root, int val){
+    if(!root){
+        Node* temp = new Node(val);
+        return temp;
+    }
+    if(root->val>val)
+    root->left = insert(root->left,val);
+    else
+    root->right = insert(root->right,val);
+
+    return root;
+}
+
+// using loop
+Node* insertLoop(Node* root, int val){
+    Node* temp = new Node(val);
+    if(!root){
+        return temp;
+    }
+    Node* ptr = root;
+    while(ptr){
+        if(ptr->val>val){
+            if(!ptr->left){
+                ptr->left = temp;
+                break;
+            }
+            else{
+                ptr=ptr->left;
+            }
+        }
+        else{
+            if(!ptr->right){
+                ptr->right = temp;
+                break;
+            }   
+            else{
+                ptr=ptr->right;
+            }
+        }
+    }
+    return root;
+}
+
+int main(){
+    int arr[6] = {5,4,2,1,25,9};
+
+    Node* root = NULL;
+
+    for(int i=0;i<6;i++){
+        root=insert(root,arr[i]);
+    }
+
+    inOrder(root);
     return 0;   
 }
