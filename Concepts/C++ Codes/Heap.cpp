611e7a8f764e9121be0c157247a508fd41c60d94
BlackEmpir7199 <blackempire9177@gmail.com>
2025-08-10 23:56:15 +0530

    starting heaps

---
+++ Concepts/C++ Codes/Heap.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node *left = NULL, *right = NULL;
+
+        Node(int data):data(data){}
+};
+
+
+
+
+int main(){
+
+}   
