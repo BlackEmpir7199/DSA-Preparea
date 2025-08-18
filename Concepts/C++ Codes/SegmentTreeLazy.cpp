75604111d4cd303834b464a3beebfa9e8871a10a
BlackEmpir7199 <blackempire9177@gmail.com>
2025-08-18 22:50:18 +0530

    Segment Tree with lazy propagtion impl

--- Concepts/C++ Codes/SegmentTreeLazy.cpp
+++ Concepts/C++ Codes/SegmentTreeLazy.cpp
@@ -31,6 +31,60 @@ private:
         /*
         In propagate we, find any lazy node should be updated , and if yes, then we update it, and propagte the changes to the child and then remove the current lazy node, set it to zero
         */
+       if(lazy[node]!=0){
+            tree[node]+=lazy[node]*(end-start+1);
+
+            // mark their children for updates
+            if(end!=start){
+                lazy[node*2+1] += lazy[node];
+                lazy[node*2+2] += lazy[node];
+            }
+
+            lazy[node]=0;
+       }
+    }
+
+    // to perform range updates
+    void update(int node, int start, int end, int l, int r, int val){
+        propagate(node,start,end);
+
+        // no overlap
+        if(end<l || start>r) return;
+
+        // complete overlap
+        if(l<=start && r>=end){
+            tree[node]=val*(end-start+1);
+
+            if(start!=end){
+                lazy[node*2+1]+=val;
+                lazy[node*2+2]+=val;
+            }
+            return;
+        }
+
+        // partial overlap
+        int mid = (start+end)/2;
+        update(node*2+1,start,mid,l,r,val);
+        update(node*2+2,mid+1,end,l,r,val);
+        tree[node]=tree[node*2+1]+tree[node*2+2];
+
+    }
+
+    int query(int node, int start, int end, int l, int r){
+        propagate(node,start,end);
+        // no overlap
+        if(end<l || start>r) return 0;
+
+        // complete overlap
+        if(l<=start && r>=end){
+            return tree[node];
+        }
+
+        int mid = (start+end)/2;
+        int lSum = query(node*2+2,start,mid,l,r);
+        int rSum = query(node*2+1,mid+1,end,l,r);
+        return lSum+rSum;
+        
     }
 
 public:
